70cde80b9e32bb2aabe2b3e1aeb04c4d4f6842c1
nothingisit4me <slee435@ucmerced.edu>
2020-11-27 13:47:14 -0800

    learning react stuff

---
+++ art-supply/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './style.module.css'
+
+
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <h1 className = {styles.header}>Art Supply Database</h1>
+    
+      </div>
+    );
+  }
+}
+
+export default App;
+
