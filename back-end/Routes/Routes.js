3ed04c1784e54f886a444c602ae392fff60c0a86
TheRealLeah <72580759+TheRealLeah@users.noreply.github.com>
2020-12-09 21:54:07 -0800

    Add files via upload

--- back-end/Routes/Routes.js
+++ back-end/Routes/Routes.js
@@ -1,10 +1,14 @@
 import express from 'express';
-import {Products, EditProduct, DeleteProduct, AddProduct} from '../Connection/Connection.js';
+import {Products, EditProduct, DeleteProduct, AddProduct, Orders, Store} from '../Connection/Connection.js';
 
 const routes = express.Router();
 
 //This makes the routes to the front-end code. Shows up as http://localhost:4006/Products in the front end components classes. 
 routes.get('/Products', Products);
+
+routes.get('/Orders', Orders);
+
+routes.get('/Store', Store);
 
 routes.put('/Edit', EditProduct);
 
